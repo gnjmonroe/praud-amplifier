47e13e531e46000d223b1631d76f8b214ab5f610
gnjmonroe <gnjmonroe@gmail.com>
2022-12-03 11:03:06 +0900

    second disable attempt.

--- src/main.tsx
+++ src/main.tsx
@@ -1,99 +1,101 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import Home from './pages/Home';
-// import Result from './pages/Result';
-// import Confirm from './pages/Confirm';
-// import CategoryPage from './pages/CategoryPage';
-// import NotFound from './pages/NotFound';
-// import './scss/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import Confirm from './pages/Confirm';
+import CategoryPage from './pages/CategoryPage';
+import NotFound from './pages/NotFound';
+import './scss/main.scss';
 
-// // categories and their options as an object[]
-// const data = [
-//   {
-//     category: 'Model',
-//     options: [
-//       'Pool',
-//       'Cinema',
-//       'Kitchen',
-//       'Library',
-//       'Garden',
-//     ],
-//   },
-//   {
-//     category: 'Location',
-//     options: [
-//       'Mountain',
-//       'River',
-//       'Sea',
-//       'Rural',
-//       'Valley',
-//     ],
-//   },
-//   {
-//     category: 'User',
-//     options: [
-//       'Single',
-//       'Couple',
-//       'Family',
-//       'Pet',
-//     ],
-//   },
-//   {
-//     category: 'Mood',
-//     options: [
-//       'Active',
-//       'Silent',
-//     ],
-//   },
-//   {
-//     category: 'Opening',
-//     options: [
-//       'Windowless',
-//       'Scenic',
-//       'Skylight',
-//       'Folding',
-//     ],
-//   },
-// ];
+// categories and their options as an object[]
+const data = [
+  {
+    category: 'Model',
+    options: [
+      'Pool',
+      'Cinema',
+      'Kitchen',
+      'Library',
+      'Garden',
+    ],
+  },
+  {
+    category: 'Location',
+    options: [
+      'Mountain',
+      'River',
+      'Sea',
+      'Rural',
+      'Valley',
+    ],
+  },
+  {
+    category: 'User',
+    options: [
+      'Single',
+      'Couple',
+      'Family',
+      'Pet',
+    ],
+  },
+  {
+    category: 'Mood',
+    options: [
+      'Active',
+      'Silent',
+    ],
+  },
+  {
+    category: 'Opening',
+    options: [
+      'Windowless',
+      'Scenic',
+      'Skylight',
+      'Folding',
+    ],
+  },
+];
 
-// // makes a route for each category
-// const dataAsRoutes = data.map((set: any, i: number) => ({
-//   path: `/${set.category.toLowerCase()}`,
-//   element: <CategoryPage
-//     data={data}
-//     dataIndex={i}
-//     prevSlug={data[(i - 1)]?.category.toLowerCase() || ''}
-//     nextSlug={data[(i + 1)]?.category.toLowerCase() || 'confirm'}
-//   />,
-//   errorElement: <NotFound />,
-// }));
+// makes a route for each category
+const dataAsRoutes = data.map((set: any, i: number) => ({
+  path: `/${set.category.toLowerCase()}`,
+  element: <CategoryPage
+    data={data}
+    dataIndex={i}
+    prevSlug={data[(i - 1)]?.category.toLowerCase() || ''}
+    nextSlug={data[(i + 1)]?.category.toLowerCase() || 'confirm'}
+  />,
+  errorElement: <NotFound />,
+}));
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Home
-//       destination={data[0].category}
-//     />,
-//     errorElement: <NotFound />,
-//   },
-//   {
-//     path: '/confirm',
-//     element: <Confirm data={data} />,
-//     errorElement: <NotFound />,
-//   },
-//   {
-//     path: '/result',
-//     element: <Result />,
-//     errorElement: <NotFound />,
-//   },
-//   ...dataAsRoutes,
-// ]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home
+      destination={data[0].category}
+    />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: '/confirm',
+    element: <Confirm data={data} />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: '/result',
+    element: <Result />,
+    errorElement: <NotFound />,
+  },
+  ...dataAsRoutes,
+]);
 
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <div className="container">
-//       <RouterProvider router={router} />
-//     </div>
-//   </React.StrictMode>,
-// );
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    {typeof data === 'object' ? (
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    ) : ''}
+  </React.StrictMode>,
+);
